06237e05a43d2ddc6738250806b2842fc307344d
parksuhyun9321 <59465364+parksuhyun9321@users.noreply.github.com>
2021-11-04 16:06:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,11 @@
 let loading_color = 1;
 let loading_second = 0;
-let deviceUA = navigator.userAgent.toLowerCase();
-let about_st;
-
-let date = new Date();
-let today = date.getDate();
-
-if (localStorage.getItem("cookie") != today) { // 쿠키생성일과 오늘의 날짜가 다를시 쿠키를 지움
-    localStorage.removeItem("cookie");
-}
-
-if (localStorage.getItem("cookie") != null) { // 쿠키가 있을시 팝업창을 띄우지 않음
-    document.querySelector("#popup").remove();
-} else {// 쿠키가 없을시
-    dragElement(document.getElementById("popup"));
-    window.addEventListener("resize", function () {
-        if (window.innerWidth <= 750) { // 윈도우 사이즈가 750보다 작을시 드래그 이벤트를 실행하지 않음
-            dragElement();
-        } else {
-            dragElement(document.getElementById("popup"));
-        }
-    });
-}
-
 window.addEventListener("load", function () {
-    if ( deviceUA.indexOf("android") > -1 || deviceUA.indexOf("iphone") > -1 || deviceUA.indexOf("ipad") > -1 || deviceUA.indexOf("ipod") > -1) { // 모바일 구분
-        let transparent_area = document.createElement("div");
-        transparent_area.classList.add("transparent_area");
-        document.querySelector(".skip").before(transparent_area);
-        document.querySelector(".cursor").remove();
-        about_st = 2;
-    } else { // pc
-        about_st = 4;
-        window.addEventListener("mousemove", function (e) { // 마우스 따라다니기
-            document.querySelector(".cursor").style.top = e.pageY + "px";
-            document.querySelector(".cursor").style.left = e.pageX + "px";
-        });
-    }
-
-    let second = setInterval(() => { // 로딩 화면
+    let second = setInterval(() => {
         if (loading_second < 100) {
             loading_second++;
             document.querySelector(".loading_second").innerHTML = `${loading_second}%`;
-        } else if (loading_second == 100) { // 로딩 100% 가 되면
+        } else if (loading_second == 100) {
             clearInterval(second);
             document.querySelector(".loading_second").remove();
             let background_set = setInterval(() => {
@@ -52,10 +15,6 @@ window.addEventListener("load", function () {
                     if (Math.floor(loading_color) == -1) {
                         document.querySelector("body").classList.remove("loading");
                         clearInterval(background_set);
-                        this.document.querySelector("#header").style.opacity = 1;
-                        if(document.querySelector(".transparent_area") != null){ // 모바일일때
-                            document.querySelector(".transparent_area").remove();
-                        }
                     }
                 }
             }, 100);
@@ -63,8 +22,7 @@ window.addEventListener("load", function () {
     }, 30);
 });
 
-
-
+let about_st = 4;
 window.addEventListener("scroll", function () {//스크롤링 이벤트
     let st = (document.documentElement.scrollTop || window.scrollY || window.pageYOffset) + window.innerHeight;
     if (st >= document.querySelector(".skill_view").offsetTop * about_st) {
@@ -79,9 +37,9 @@ window.addEventListener("scroll", function () {//스크롤링 이벤트
         document.querySelector("#portfolio .portfolio_list").classList.remove("show");
     }
 
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) { // 맨아래로 내려왔을시
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         document.querySelector(".btn_top").classList.add("on");
-        if (document.querySelector("#popup") != null) {
+        if(document.querySelector("#popup") != null){
             document.querySelector("#popup").classList.add("show");
         }
     } else {
@@ -91,44 +49,45 @@ window.addEventListener("scroll", function () {//스크롤링 이벤트
 
 let skill_btn = document.querySelectorAll(".skill_list li button"); // 스킬버튼 클릭시 해당 내용과 사진을 띄움
 
-for (let i = 0; i < skill_btn.length; i++) { // 스킬버튼을 클릭시 이벤트
+for (let i = 0; i < skill_btn.length; i++) {
     skill_btn[i].addEventListener("click", function () {
         for (let j = 0; j < skill_btn.length; j++) {
             skill_btn[j].classList.remove("on");
         }
         let src = this.value;
-        let txt_alt = this.parentNode.children[1].textContent.trim();
-
+        let alt = this.textContent.replace("#", "").toLowerCase();
+        let txt = this.parentNode.children[1].textContent.trim();
+        
         this.classList.add("on");
         document.querySelector(".skill_view img").src = src;
-        document.querySelector(".skill_view img").alt = txt_alt;
-        document.querySelector(".skill_view p").innerHTML = txt_alt;
+        document.querySelector(".skill_view img").alt = alt;
+        document.querySelector(".skill_view p").innerHTML = txt;
     });
 }
 
-document.querySelector(".btn_gnb").addEventListener("click", function () { // 클릭시 gnb 메뉴 오픈
+document.querySelector(".btn_gnb").addEventListener("click", function () {
     document.querySelector(".gnb").classList.add("on");
-    this.style.display = "none";
+    this.style.display="none";
 });
 
-document.querySelector(".btn_close").addEventListener("click", function () { // 클릭시 gnb 메뉴 닫음
+document.querySelector(".btn_close").addEventListener("click", function () {
     document.querySelector(".gnb").classList.remove("on");
-    document.querySelector(".btn_gnb").style.display = "block";
+    document.querySelector(".btn_gnb").style.display="block";
 });
 
 let gnb_btn = document.querySelectorAll(".gnb_list li a");
 
-for (let i = 0; i < gnb_btn.length; i++) { // 클릭시 해당 섹션으로 스크롤
+for (let i = 0; i < gnb_btn.length; i++) {
     gnb_btn[i].addEventListener("click", function () {
-        smoothScroll(document.getElementById(`${this.dataset.target}`).offsetTop);
         document.querySelector(".gnb").classList.remove("on");
-        document.querySelector(".btn_gnb").style.display = "block";
+        document.querySelector(".btn_gnb").style.display="block";
+        if(this.textContent == "HOME"){
+            smoothScroll(document.getElementById("main_visual").offsetTop);
+        } else {
+            smoothScroll(document.getElementById(`${this.textContent.toLowerCase()}`).offsetTop);
+        }
     });
 }
-
-document.querySelector(".btn_top").addEventListener("click", function () { // 클릭시 맨위로 스크롤
-    smoothScroll(0);
-});
 
 function smoothScroll(a) {
     const targetPosition = a;
@@ -154,74 +113,109 @@ function easeInOutCubic(t, b, c, d) {
     return (c / 2) * (t * t * t + 2) + b;
 }
 
-document.querySelectorAll(".item").forEach((items) => { // 버튼에 마우스를 올릴시 마우스효과
-    let class_name = items.textContent;
-    items.addEventListener("mouseenter", function () {
-        document.querySelector(".cursor").classList.add("active");
-    });
-    items.addEventListener("mouseleave", function () {
-        document.querySelector(".cursor").classList.remove("active");
-    });
+document.querySelector(".btn_top").addEventListener("click", function () { // 클릭시 맨위로 이동
+    smoothScroll("0");
 });
 
-// gnb 메뉴에 마우스를 올릴시 색상 변경
-document.querySelector(".gnb").addEventListener("mouseenter", function () {
-    document.querySelector(".cursor").classList.add("white");
-});
-document.querySelector(".gnb").addEventListener("mouseleave", function () {
-    document.querySelector(".cursor").classList.remove("white");
-});
+let deviceUA = navigator.userAgent.toLowerCase();
 
-// 포트폴리오 팝업창 하단 버튼
-document.querySelectorAll("#popup .btn_list li button").forEach((item) => {
-    item.addEventListener("click", function () {
+if (deviceUA.indexOf("android") > -1 ||deviceUA.indexOf("iphone") > -1 ||deviceUA.indexOf("ipad") > -1 ||deviceUA.indexOf("ipod") > -1) {
+    document.querySelector(".cursor").remove();
+    about_st = 2;
+} else {
+    window.addEventListener("mousemove", function (e) {
+        document.querySelector(".cursor").style.top = e.pageY + "px";
+        document.querySelector(".cursor").style.left = e.pageX + "px";
+    });
+
+    document.querySelectorAll(".item").forEach((items) => {
+        items.addEventListener("mouseenter", function () {
+            document.querySelector(".cursor").classList.add("active");
+        });
+        items.addEventListener("mouseleave", function () {
+            document.querySelector(".cursor").classList.remove("active");
+        });
+    });
+
+    document.querySelector(".gnb").addEventListener("mouseenter", function () {
+        document.querySelector(".cursor").style.backgroundColor = "rgba(255,255,255,0.8)";
+    });
+    document.querySelector(".gnb").addEventListener("mouseleave", function () {
+        document.querySelector(".cursor").style.backgroundColor = "rgba(0,0,0,0.8)";
+    });
+
+    if (document.querySelector(".loading-second") != null) {
+        document.querySelector(".loading-second").addEventListener("mouseenter", function () {
+            document.querySelector(".loading-second").style.color = "#000";
+        });
+    }
+}
+
+let date = new Date();
+let today = date.getDate();
+
+if (localStorage.getItem("cookie") != today) {
+    localStorage.removeItem("cookie");
+}
+
+if (localStorage.getItem("cookie") != null) {
+    document.querySelector("#popup").remove();
+} else {
+    dragElement(document.getElementById("popup")); 
+    window.addEventListener("resize",function(){
+        if(window.innerWidth <= 750){
+            dragElement("none")
+        }
+    })
+}
+
+document.querySelectorAll("#popup .btn_list li button").forEach((item)=>{
+    item.addEventListener("click",function(){
         document.querySelector("#popup").remove();
-        if (item.className.indexOf("today_hide") != -1) {// 오늘하루 보지 않기 클릭시 쿠키 생성
+        if(item.className.indexOf("today_hide") != -1){
             localStorage.setItem("cookie", today);
         }
-    });
+    })
 });
 
+
 function dragElement(elmnt) {
-    var pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (elmnt.id ==  "popup") {
+      /* if present, the header is where you move the DIV from:*/
+      document.getElementById(elmnt.id).onmousedown = dragMouseDown;
     } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.onmousedown = dragMouseDown;
+      /* otherwise, move the DIV from anywhere inside the DIV:*/
+      elmnt.onmousedown = dragMouseDown;
     }
-
+  
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
     }
-
+  
     function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
-
+  
     function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
     }
 }
